1fd49a43af2da171a8e4853de2a8799a3de50dd0
nasrat-noury <nasrat.noury@gmail.com>
2022-02-18 12:49:21 +0100

    corrections

--- portefolio/src/components/Projet.jsx
+++ portefolio/src/components/Projet.jsx
@@ -3,36 +3,35 @@ import axios from "axios";
 import "../style/Projet.css";
 
 const Projet = () => {
-  const [Projet, setProjet] = useState([]);
+  //const [Projet, setProjet] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:3001/api/projets`)
-      .then((response) => setProjet(response.data));
-  }, []);
+  //useEffect(() => {
+  //  axios
+  //    .get(`http://localhost:3001/api/projets`)
+  //    .then((response) => setProjet(response.data));
+  //}, []);
 
   return (
     <div className="ProjetPage">
       <div className="ProjetCards" >
-        {Projet &&
-          Projet.map((projet) => (
+        
             <div className="ProjCard">
               <div className="DescriptionProjet">
                 <div className="CardImage">
                   <img
-                    src={projet.img}
-                    alt={projet.name}
+                    src="https://imgur.com/jgWqkgD.png"
+                    alt="Festoch'Fastoch"
                     className="ImageProjet"
                   />
                 </div>
                 <div className="CardText">
-                  <h2>{projet.name}</h2>
-                  <h3>{projet.date}</h3>
-                  <h4>{projet.description}</h4>
-                  <h5>{projet.tech}</h5>
+                  <h2>Festoch'Fastoch</h2>
+                  <h3>"Octobre/Novembre 2020"</h3>
+                  <h4>Festoch Fastoch est un site de billeterie en ligne. Il y a plusieurs style de musique disponible selon les préférences de chacun</h4>
+                  <h5>React.js, Node.js, Express, MySQL, Workbench, Postman</h5>
                   <h5>
                     <a
-                      href={projet.link}
+                      href="https://festochfastoch.herokuapp.com/"
                       className="projet-linkto"
                       target="blank"
                       rel="noopener noreferer"
@@ -43,7 +42,88 @@ const Projet = () => {
                 </div>
               </div>
             </div>
-          ))}
+            <div className="ProjCard">
+              <div className="DescriptionProjet">
+                <div className="CardImage">
+                  <img
+                    src="https://i.imgur.com/or7a6n1.png?1"
+                    alt="Ridy"
+                    className="ImageProjet"
+                  />
+                </div>
+                <div className="CardText">
+                  <h2>Ridy</h2>
+                  <h3>Hackathon 24h</h3>
+                  <h4>Back to the future ! Hackathon de 24h sur la création d'une application permettant de louer des chevaux. Europcar mais avec des chevaux !</h4>
+                  <h5>React.js, Node.js, Express, mySQL, Google maps API</h5>
+                  <h5>
+                    <a
+                      href="https://ridy.herokuapp.com/"
+                      className="projet-linkto"
+                      target="blank"
+                      rel="noopener noreferer"
+                    >
+                      Voir le projet
+                    </a>
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div className="ProjCard">
+              <div className="DescriptionProjet">
+                <div className="CardImage">
+                  <img
+                    src="https://i.imgur.com/Sinvy2a.png"
+                    alt="Cook'Eat"
+                    className="ImageProjet"
+                  />
+                </div>
+                <div className="CardText">
+                  <h2>Cook'Eat</h2>
+                  <h3>Hackathon 48h Janvier 2021</h3>
+                  <h4>Hackathon organisé par Ekino et la Wild Code School de Bordeaux sur le sujet "Créer du lien en période de confinement". Cook'Eat est une application de diffusion en direct de recettes de cuisine par les utilisateurs de cette app.</h4>
+                  <h5>React.js, Node.js, Express, JWT, MySQL, Workbench, Postman, Twilio, Firebase</h5>
+                  <h5>
+                    <a
+                      href="https://cook-eat.netlify.app/"
+                      className="projet-linkto"
+                      target="blank"
+                      rel="noopener noreferer"
+                    >
+                      Voir le projet
+                    </a>
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div className="ProjCard">
+              <div className="DescriptionProjet">
+                <div className="CardImage">
+                  <img
+                    src="https://i.imgur.com/O53jTAph.jpg"
+                    alt="JonPak"
+                    className="ImageProjet"
+                  />
+                </div>
+                <div className="CardText">
+                  <h2>Jonathan Pak</h2>
+                  <h3>Décembre 2020 à Janvier 2021</h3>
+                  <h4>La web app Jonathan Pak répond à la demande d'un client qui est coach sportif, de communiquer avec ses clients. Via cette app, un coach peut envoyer à ses clients un programme sportif spécifique et de suivre leurs résultats.</h4>
+                  <h5>React.js, Redux, Node.js, Express, JWT, MySQL, Workbench, Postman</h5>
+                  <h5>
+                    <a
+                      href="https://client-jonathanpak.herokuapp.com/"
+                      className="projet-linkto"
+                      target="blank"
+                      rel="noopener noreferer"
+                    >
+                      Voir le projet
+                    </a>
+                  </h5>
+                </div>
+              </div>
+            </div>
+          
       </div>
     </div>
   );
