53e496e8ec0fd09dda42b15824e9b1bad9deda32
nasrat-noury <nasrat.noury@gmail.com>
2022-02-18 20:22:39 +0100

    changes

--- portefolio/src/components/HomePage.jsx
+++ portefolio/src/components/HomePage.jsx
@@ -7,7 +7,7 @@ import img from"./fuji.png";
 const HomePage = () => {
   return (
     <div className="page-container">
-
+      <div className="left">
     <div className="titre">
       <h2 className="title">
         <p>Nasrat NOURY</p>
@@ -18,6 +18,13 @@ const HomePage = () => {
      <div><h1 className="img">
         <img src={img} alt="portrait"></img>
       </h1> </div>
+      </div>
+    <div className="right">Je me présente, je m'appelle Nasrat NOURY et je suis un développeur web junior.
+    <br></br>
+    <br></br>
+    Après une formation à la Wild Code School de Bordeaux qui m'a permis d'apprendre les différents languages du web et leur utilisation à travers différents cas pratiques. 
+    <br></br>
+    Je suis aujourd'hui à la recherche d'une nouvelle opportunité professionnelle.</div>
       
 
      
