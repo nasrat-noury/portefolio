f5732a3018f8ed29d06a2174cf7c598c74dddf6c
nasrat-noury <nasrat.noury@gmail.com>
2022-02-18 13:04:47 +0100

    allok3

--- portefolio/src/index.js
+++ portefolio/src/index.js
@@ -1,23 +1,20 @@
 import React from "react";
-import { PersistGate } from "redux-persist/integration/react";
+
 import ReactDOM from "react-dom";
 
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
+
 
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    
         <Router>
           <App />
         </Router>
-      </PersistGate>
-    </Provider>
+      
   </React.StrictMode>,
   document.getElementById("root")
 );
